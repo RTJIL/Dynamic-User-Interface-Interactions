1e93370e7ab255218a0bf00d9c499fdf365af975
Artem <161304626+RTJIL@users.noreply.github.com>
2025-05-23 15:48:00 +0000

    make dropdown.js that implement dropdown logic

---
+++ src/dropdown.js
@@ -0,0 +1,19 @@
+function dropdown() {
+    document.addEventListener("click", e => {
+        const isDropDownButton = e.target.matches("[data-dropdown-button]")
+        if (!isDropDownButton && e.target.closest('[data-dropdown]') != null) return
+
+        let currentDropdown
+        if(isDropDownButton) {
+            currentDropdown = e.target.closest('[data-dropdown]')
+            currentDropdown.classList.toggle('active')
+        }
+
+        document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+            if (dropdown === currentDropdown) return
+            dropdown.classList.remove('active')
+        })
+    })
+} 
+
+export { dropdown }
